6e790f4d537e0b03bd61714021034e6aa7d6cf7f
Atr0fy <2006lucasns@gmail.com>
2024-08-24 13:55:42 -0300

    codigo dos botoes

--- PAM-TipCalculator/MainPage.xaml.cs
+++ PAM-TipCalculator/MainPage.xaml.cs
@@ -9,12 +9,14 @@
 
         private void SetLowTip_Clicked(object sender, EventArgs e)
         {
-            
+            TipSlider.Value = 15;
+            TipPercentLabel.Text = TipSlider.Value.ToString() + "%";
         }
 
         private void SetHighTip_Clicked(object sender, EventArgs e)
         {
-         
+            TipSlider.Value = 20;
+            TipPercentLabel.Text = TipSlider.Value.ToString() + "%";
         }
 
         private void RoundUpButton_Clicked(object sender, EventArgs e)
@@ -29,11 +31,21 @@
 
         private void TipSlider_ValueChanged(object sender, ValueChangedEventArgs e)
         {
-           
+            TipPercentLabel.Text = TipSlider.Value.ToString("#.##") + "%";
         }
         private void AmountEntry_TextChanged(object sender, TextChangedEventArgs e)
         {
-           
+            /*
+            Pegar o valor inserido ex: 100
+            Calcular o valor da gorjeta de acordo com a porcentagem
+            Calcular o valor total, sendo a soma do valor + a gorjeta
+            */
+            double Valor = Convert.ToDouble(AmountEntry.Text);
+            double ValorDaGorjeta = Valor * (TipSlider.Value / 100);
+            double ValorTotal = ValorDaGorjeta + Valor;
+
+            TotalLabel.Text = ValorTotal.ToString("C");
+            TipLabel.Text = ValorDaGorjeta.ToString("C");
         }
     }
 
