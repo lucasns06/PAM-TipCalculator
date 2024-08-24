1dd53fe757641b9131d88b3447a5fceadbf4a70a
Atr0fy <2006lucasns@gmail.com>
2024-08-24 13:50:16 -0300

    Tela inicial feita

---
+++ PAM-TipCalculator/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿namespace PAM_TipCalculator
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void SetLowTip_Clicked(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void SetHighTip_Clicked(object sender, EventArgs e)
+        {
+         
+        }
+
+        private void RoundUpButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RoundDownButton_Clicked(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TipSlider_ValueChanged(object sender, ValueChangedEventArgs e)
+        {
+           
+        }
+        private void AmountEntry_TextChanged(object sender, TextChangedEventArgs e)
+        {
+           
+        }
+    }
+
+}
