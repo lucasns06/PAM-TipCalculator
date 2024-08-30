aae52f215534535accf5f733dab43a30c494bd84
lucasns06 <2006lucasns@gmail.com>
2024-08-30 20:46:29 -0300

    Arredondamento

--- PAM-TipCalculator/MainPage.xaml.cs
+++ PAM-TipCalculator/MainPage.xaml.cs
@@ -6,6 +6,9 @@
         {
             InitializeComponent();
         }
+        double valorTotal = 0;
+        double valorDaGorjeta = 0;
+        double valor = 0;
 
         private void SetLowTip_Clicked(object sender, EventArgs e)
         {
@@ -22,16 +25,36 @@
         private void RoundUpButton_Clicked(object sender, EventArgs e)
         {
 
+            valorDaGorjeta = Math.Ceiling(valorDaGorjeta);
+            valorTotal = Math.Ceiling(valorTotal);
+            TipLabel.Text = valorDaGorjeta.ToString("C");
+            TotalLabel.Text = valorTotal.ToString("C");
         }
 
         private void RoundDownButton_Clicked(object sender, EventArgs e)
         {
-
+            valorDaGorjeta = Math.Floor(valorDaGorjeta);
+            valorTotal = Math.Floor(valorTotal);
+            TipLabel.Text = valorDaGorjeta.ToString("C");
+            TotalLabel.Text = valorTotal.ToString("C");
         }
 
         private void TipSlider_ValueChanged(object sender, ValueChangedEventArgs e)
         {
             TipPercentLabel.Text = TipSlider.Value.ToString("#.##") + "%";
+            try
+            {
+                valor = Convert.ToDouble(AmountEntry.Text);
+                valorDaGorjeta = valor * (TipSlider.Value / 100);
+                valorTotal = valorDaGorjeta + valor;
+
+                TotalLabel.Text = valorTotal.ToString("C");
+                TipLabel.Text = valorDaGorjeta.ToString("C");
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("ERRO!", "O valor tem que ser numérico", "OK");
+            }
         }
         private void AmountEntry_TextChanged(object sender, TextChangedEventArgs e)
         {
@@ -40,13 +63,21 @@
             Calcular o valor da gorjeta de acordo com a porcentagem
             Calcular o valor total, sendo a soma do valor + a gorjeta
             */
-            double Valor = Convert.ToDouble(AmountEntry.Text);
-            double ValorDaGorjeta = Valor * (TipSlider.Value / 100);
-            double ValorTotal = ValorDaGorjeta + Valor;
+            try
+            {
+                valor = Convert.ToDouble(AmountEntry.Text);
+                valorDaGorjeta = valor * (TipSlider.Value / 100);
+                valorTotal = valorDaGorjeta + valor;
 
-            TotalLabel.Text = ValorTotal.ToString("C");
-            TipLabel.Text = ValorDaGorjeta.ToString("C");
+                TotalLabel.Text = valorTotal.ToString("C");
+                TipLabel.Text = valorDaGorjeta.ToString("C");
+            }
+            catch(Exception ex) 
+            {
+                DisplayAlert("ERRO!", "O valor tem que ser numérico", "OK");
+            }
         }
+
     }
 
 }
