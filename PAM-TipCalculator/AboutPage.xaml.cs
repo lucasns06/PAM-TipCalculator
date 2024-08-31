b4d860d2c514faf4702b976b1e4641bf7262d342
lucasns06 <2006lucasns@gmail.com>
2024-08-30 22:47:14 -0300

    funcao dos botao

--- PAM-TipCalculator/AboutPage.xaml.cs
+++ PAM-TipCalculator/AboutPage.xaml.cs
@@ -6,4 +6,14 @@ public partial class AboutPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void AboutButton_Clicked(object sender, EventArgs e)
+    {
+        await Launcher.Default.OpenAsync("https://github.com/lucasns06");
+    }
+
+    private async void SteamButton_Clicked(object sender, EventArgs e)
+    {
+        await Launcher.Default.OpenAsync("https://steamcommunity.com/id/Atrofy/");
+    }
 }
